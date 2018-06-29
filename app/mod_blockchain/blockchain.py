b279eb3dc19c15fd82e9d8279667f79fe0559e24
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-29 12:43:51 +0300

    Add hash and new block impl

--- app/mod_blockchain/blockchain.py
+++ app/mod_blockchain/blockchain.py
@@ -14,6 +14,9 @@ block = {
     'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
 }
 """
+import hashlib
+import json
+from time import time
 
 
 class Blockchain(object):
@@ -22,15 +25,40 @@ class Blockchain(object):
     """
 
     def __init__(self):
+        """
+        Creates a new instance of a Blockchain. When instantiated, a new genesis block is created (Block without any
+        predecessors), we will also need to add proof to the genesis block
+        """
         self.current_transactions = []
         self.chain = []
 
-    def new_block(self):
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
         """
         creates a new block and adds it to the chain
-        :return:
+        :param previous_hash: (Optional) Previous hash of the block in the chain
+        :type previous_hash int
+        :param proof Proof given by proof of work algorithm
+        :type proof int
+        :return: new Block
+        :rtype: dict
         """
-        pass
+        block = dict(
+            index=len(self.chain) + 1,
+            timestamp=time(),
+            transactions=self.current_transactions,
+            proof=proof,
+            previous_hash=previous_hash or self.hash(self.chain[-1])
+        )
+
+        # reset the current list of transactions
+        self.current_transactions = []
+
+        # add the block to the chain
+        self.chain.append(block)
+
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -55,13 +83,17 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         """
-
+        Creates a SHA-256 hash of a block
         :param block:
-        :return:
+        :type block dict
+        :return: Hash of the given block
+        :rtype: str
         """
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         """Returns the last block on the chain"""
-        pass
+        return self.chain[-1]
+
