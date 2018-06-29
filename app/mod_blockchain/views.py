d9f5e9cc30b23bf569e2515f831e02182f74b22a
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-29 12:57:20 +0300

    Views route handling mine, get_chain and new_transaction

--- app/mod_blockchain/views.py
+++ app/mod_blockchain/views.py
@@ -1,2 +1,29 @@
 from . import block
 from flask import jsonify, request
+from uuid import uuid4
+from .blockchain import Blockchain
+
+# generates a globally unique address for this node
+node_identifier = str(uuid4()).replace("-", "")
+
+blockchain = Blockchain()
+
+
+@block.route("/transactions/new", methods=["POST"])
+def new_transaction():
+    return "Add a new transaction"
+
+
+@block.route("/mine", methods=["POST"])
+def mine_block():
+    return "Mine a new block"
+
+
+@block.route("/chain", methods=["GET"])
+def get_chain():
+    response = dict(
+        chain=blockchain.chain,
+        length=len(blockchain.chain)
+    )
+    return jsonify(response), 200
+
