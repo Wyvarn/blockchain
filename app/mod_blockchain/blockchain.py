e4ed07e26bc2af02f6bea3d9d3a0271c2c284d36
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-29 12:48:36 +0300

    Proof of work algorithm

--- app/mod_blockchain/blockchain.py
+++ app/mod_blockchain/blockchain.py
@@ -97,3 +97,34 @@ class Blockchain(object):
         """Returns the last block on the chain"""
         return self.chain[-1]
 
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof
+        :type last_proof int
+        :return: Proof of work
+        :rtype: int
+        """
+        proof = 0
+
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :type last_proof int
+        :param proof: Current Proof
+        :type proof int
+        :return: True if correct, False if not.
+        :rtype: bool
+        """
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
